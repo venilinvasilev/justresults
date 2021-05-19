b38fd7c5b0e77ade8106b5fc67129860d065377e
phatyg <venilin.vasilev@gmail.com>
2021-05-19 09:38:59 +0300

    Added some html & css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="bg-image">
+            <div className="site-wrapper">
+                <header>
+                    <div className="header-container">
+                        <div className="site-logo">#GymTips</div>
+                        <nav>
+                            <a className="site-nav-link" href="index.html">Home</a>
+                            <a className="site-nav-link" href="index.html">Workouts</a>
+                            <a className="site-nav-link" href="index.html">Tips</a>
+                            <a className="site-nav-link" href="index.html">Calculator</a>
+                        </nav>
+                    </div>
+                </header>
+                <hr className="separation-line" />
+                <div className="site-content">
+                    <h1 className="home-heading">Popular Tips</h1>
+                    <section>
+                        <div className="sections-wrapper">
+                            <div className="popular-article">
+                                <h2 className="article-header">Intermittent Fasting: Fat-Burning Blitz</h2>
+                                <p className="short-description">
+                                    Two things in particular are very important to me: Eating the foods I love and
+                                    staying lean.
+                                    If I feel like eating donuts, I eat donuts. When I want a beer, a margarita or a
+                                    sake bomb, I imbibe. If I'm at a restaurant, I'll get a steak and some delicious
+                                    sides, then finish it off with dessert. In other words, I eat what I want, when I
+                                    want it. (Within reason, of course.)
+                                </p>
+                            </div>
+                            <div className="popular-article">
+                                <h2 className="article-header">Intermittent Fasting: Fat-Burning Blitz</h2>
+                                <p className="short-description">
+                                    Two things in particular are very important to me: Eating the foods I love and
+                                    staying lean.
+                                    If I feel like eating donuts, I eat donuts. When I want a beer, a margarita or a
+                                    sake bomb, I imbibe. If I'm at a restaurant, I'll get a steak and some delicious
+                                    sides, then finish it off with dessert. In other words, I eat what I want, when I
+                                    want it. (Within reason, of course.)
+                                </p>
+                            </div>
+                            <div className="popular-article">
+                                <h2 className="article-header">Intermittent Fasting: Fat-Burning Blitz</h2>
+                                <p className="short-description">
+                                    Two things in particular are very important to me: Eating the foods I love and
+                                    staying lean.
+                                    If I feel like eating donuts, I eat donuts. When I want a beer, a margarita or a
+                                    sake bomb, I imbibe. If I'm at a restaurant, I'll get a steak and some delicious
+                                    sides, then finish it off with dessert. In other words, I eat what I want, when I
+                                    want it. (Within reason, of course.)
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <footer>
+                <div className="site-footer">
+                    <p>Site created by Venko Zmeya All rights reserved</p>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
